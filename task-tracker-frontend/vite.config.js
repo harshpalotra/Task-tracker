172030eb7bbadfc6ee8e13687234fdc0745e671e
harshpalotra <palotraharsh@gmail.com>
2025-05-04 23:30:45 +0530

    add env

--- task-tracker-frontend/vite.config.js
+++ task-tracker-frontend/vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
+
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+const env = loadEnv(mode, process.cwd())
+return {
   plugins: [
     tailwindcss(),
     react()
@@ -11,11 +13,12 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: env.VITE_API_URL,
         changeOrigin: true,
         secure: false,
       },
     },
   },
+}
 });
 
